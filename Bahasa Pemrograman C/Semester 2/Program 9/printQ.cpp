21d5e2f2490bbb9b165599992e4a1abb9dc81d73
darkdanu91 <117131935+darkdanu91@users.noreply.github.com>
2023-05-10 14:19:08 +0700

    Update printQ.cpp

--- Bahasa Pemrograman C/Semester 2/Program 9/printQ.cpp
+++ Bahasa Pemrograman C/Semester 2/Program 9/printQ.cpp
@@ -1,14 +1,16 @@
 #include "function.h"
 
+// Fungsi untuk mencetak isi antrian Q ke layar
 void printQ(antrian Q) {
+    // Cek apakah antrian kosong
     if (emptyQ(Q)) {
-        printf("Antrian kosong\n");
-        return;
+        printf("Antrian kosong\n"); // jika iya, cetak "antrian kosong"
+        return; // kembalikan
     }
 
-    tInfo X;
+    tInfo X; // deklarasi
     while (!emptyQ(Q)) {
-        DeQueue(&Q, &X);
-        printf("%d ", X);
+        DeQueue(&Q, &X); // Dequeue elemen dari antrian
+        printf("%d ", X); // Cetak elemen yang di-dequeue
     }
 }
