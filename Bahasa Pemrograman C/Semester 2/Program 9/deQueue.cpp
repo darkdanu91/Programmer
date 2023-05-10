ec0f4369b237a2c5dbc40e245226b5ae04482214
darkdanu91 <117131935+darkdanu91@users.noreply.github.com>
2023-05-10 13:47:08 +0700

    Update deQueue.cpp

--- Bahasa Pemrograman C/Semester 2/Program 9/deQueue.cpp
+++ Bahasa Pemrograman C/Semester 2/Program 9/deQueue.cpp
@@ -13,4 +13,15 @@ void deQueue(antrian *Q, tInfo *X) {
     (*Q).tail--;
 }
 
+/*
+Komentar:
+Fungsi ini dapat dijelaskan dengan analogi seperti mengambil buku pertama dari tumpukan buku.
+
+Kita pertama-tama mengecek apakah tumpukan buku kosong atau tidak.
+Jika kosong, maka tidak ada buku yang bisa diambil.
+Namun jika tidak kosong, kita mengambil buku paling atas atau buku pertama dari tumpukan tersebut, lalu menggeser semua buku ke bawah satu per satu.
+Setelah itu, kita menandai bahwa buku paling bawah sudah diambil dengan mengurangi jumlah buku pada tumpukan.
+
+*/
+
 
