8072a2c42e62e7100bbfaac63e06cc7c377e1930
darkdanu91 <117131935+darkdanu91@users.noreply.github.com>
2023-05-10 14:16:05 +0700

    Update main.cpp

--- Bahasa Pemrograman C/Semester 2/Program 9/main.cpp
+++ Bahasa Pemrograman C/Semester 2/Program 9/main.cpp
@@ -1,11 +1,12 @@
 #include "function.h"
-antrian Q; // global variable
+antrian Q; // variabel global
 
 int main() {
-    int choice, val;
-    createQ(&Q);
+    int choice, val; // deklarasi
+    createQ(&Q); // inisialisasi antrian Q dengan fungsi createQ()
 
     do {
+        // Menampilkan menu pilihan
         printf("\n\nPemrosesan Antrian\n");
         printf("1. Penyisipan\n");
         printf("2. Hapus\n");
@@ -13,15 +14,18 @@ int main() {
         printf("4. Keluar\n");
         printf("Pilihan [1-4]: ");
         scanf("%d", &choice);
-
+        
+        // Switch case untuk memproses pilihan menu
         switch (choice) {
             case 1:
+                // Menambahkan elemen ke antrian menggunakan fungsi enQueue()
                 printf("\nMasukkan nilai: ");
                 scanf("%d", &val);
                 enQueue(&Q, val);
                 printf("Nilai %d berhasil disisipkan ke dalam antrian.\n", val);
                 break;
             case 2:
+                // Menghapus elemen dari antrian menggunakan fungsi deQueue()
                 if (emptyQ(Q)) {
                     printf("\nAntrian kosong.\n");
                 } else {
@@ -30,13 +34,16 @@ int main() {
                 }
                 break;
             case 3:
+                // Mencetak isi antrian menggunakan fungsi printQ()
                 printf("\nIsi antrian: ");
                 printQ(Q);
                 break;
             case 4:
+                // Keluar dari program
                 printf("\nProgram selesai.\n");
                 exit(0);
             default:
+                // Jika pilihan tidak valid
                 printf("\nPilihan tidak valid.\n");
                 break;
         }
