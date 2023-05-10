9ae9e0b5076e35932b1df2326ce9781cd537d468
darkdanu91 <117131935+darkdanu91@users.noreply.github.com>
2023-05-10 13:55:36 +0700

    Update createQ.cpp

--- Bahasa Pemrograman C/Semester 2/Program 9/createQ.cpp
+++ Bahasa Pemrograman C/Semester 2/Program 9/createQ.cpp
@@ -4,3 +4,8 @@ void createQ(antrian *Q) {
     (*Q).head = 0;
     (*Q).tail = 0;
 }
+
+// Komentar:
+// Prosedur untuk membuat antrian kosong
+// Parameter Q adalah pointer ke antrian yang ingin dibuat kosong
+// Head dan tail diatur ke 0, menunjukkan antrian kosong.
