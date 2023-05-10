901ae485fa8d8cd215abda684f7abfb1f7ff87e6
darkdanu91 <117131935+darkdanu91@users.noreply.github.com>
2023-05-10 14:02:15 +0700

    Update emptyQ.cpp

--- Bahasa Pemrograman C/Semester 2/Program 9/emptyQ.cpp
+++ Bahasa Pemrograman C/Semester 2/Program 9/emptyQ.cpp
@@ -3,3 +3,9 @@
 bool emptyQ(antrian Q) {
     return (Q.head == 0 && Q.tail == 0);
 }
+
+// Komentar:
+// Fungsi untuk memeriksa apakah antrian kosong atau tidak
+// Parameter Q adalah antrian yang ingin diperiksa kekosongan
+// Antrian dianggap kosong jika head dan tail-nya berada di indeks 0. 
+// Jika antrian kosong, fungsi akan mengembalikan nilai true. Jika tidak, mengembalikan false.
