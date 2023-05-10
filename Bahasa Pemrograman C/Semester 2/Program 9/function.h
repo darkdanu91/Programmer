8765a0d3c248be716e965bc223c4d6435fba8d99
darkdanu91 <117131935+darkdanu91@users.noreply.github.com>
2023-05-10 14:10:59 +0700

    Update function.h

--- Bahasa Pemrograman C/Semester 2/Program 9/function.h
+++ Bahasa Pemrograman C/Semester 2/Program 9/function.h
@@ -1,21 +1,28 @@
+// Header
 #include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-const int nMax = 5;
-typedef int tInfo;
-typedef int address;
+// Nilai Konstan
+const int nMax = 5; // ukuran maksimal array
+
+// Tipe Data Baru
+typedef int tInfo; // tipe data: elemen antrian
+typedef int address; // tipe data: indeks antrian
 
 typedef struct {
-    address head;
-    address tail;
-    tInfo arrQ[nMax];
-} antrian;
+    address head; // indeks kepala antrian
+    address tail; // indeks ekor antrian
+    tInfo arrQ[nMax]; // panjang antrian
+} antrian; // tipe data: antrian
 
-void createQ(antrian *Q);
-bool emptyQ(antrian Q);
-void enQueue(antrian *Q, tInfo X);
-void DeQueue(antrian *Q, tInfo *X);
-void printQ(antrian Q);
+// Prosedur
+void createQ(antrian *Q); // membuat antrian kosong
+void enQueue(antrian *Q, tInfo X); // memasukkan elemen ke antrian
+void DeQueue(antrian *Q, tInfo *X); // mengeluarkan elemen dari antrian
+void printQ(antrian Q); // mencetak seluruh elemen antrian
+
+// Fungsi
+bool emptyQ(antrian Q); // memeriksa apakah antrian kosong
