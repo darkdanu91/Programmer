8f6b76af6f2ff4187dfd2f310b4b98a1d01e2fe1
darkdanu91 <117131935+darkdanu91@users.noreply.github.com>
2023-05-22 16:37:38 +0700

    Update EmptyStack.cpp

--- Bahasa Pemrograman C/Semester 2/Program 10/EmptyStack.cpp
+++ Bahasa Pemrograman C/Semester 2/Program 10/EmptyStack.cpp
@@ -1,5 +1,6 @@
 #include "function.h"
 
+// Fungsi untuk memeriksa apakah Stack kosong
 bool EmptyStack(Stack S) {
-    return (S.Top == 0);
+    return (S.Top == 0);  // Mengembalikan nilai true jika Stack kosong, false jika tidak
 }
