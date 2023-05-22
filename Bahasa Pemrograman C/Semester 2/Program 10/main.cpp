510cc6b36d301e084903df35d9de9e882c853404
darkdanu91 <117131935+darkdanu91@users.noreply.github.com>
2023-05-22 16:50:40 +0700

    Update main.cpp

--- Bahasa Pemrograman C/Semester 2/Program 10/main.cpp
+++ Bahasa Pemrograman C/Semester 2/Program 10/main.cpp
@@ -1,13 +1,14 @@
 #include "function.h"
-Stack S; // global variable
+Stack S; // Variabel global Stack
 
 int main() {
     //-declare
-    int Choice;
-    TInfo X;
+    int Choice; // Variabel untuk menyimpan pilihan pengguna
+    TInfo X; // Variabel untuk menyimpan nilai elemen
 
     //-algorithm
-    CreateStack(&S);
+    CreateStack(&S); // Menginisialisasi Stack sebelum penggunaan
+
     do {
         printf("Pemrosesan Stack\n");
         printf("1. Penyisipan\n");
@@ -16,38 +17,37 @@ int main() {
         printf("4. Selesai\n");
 
         printf("Pilihan [1/2/3/4]: ");
-        scanf("%d", &Choice);
+        scanf("%d", &Choice); // Membaca pilihan pengguna
 
         switch (Choice) {
             case 1:
                 printf("Info yang akan disisipkan? ");
-                scanf("%d", &X);
+                scanf("%d", &X); // Membaca nilai elemen yang akan disisipkan
 
-                Push(&S, X);
+                Push(&S, X); // Menyisipkan elemen ke Stack
                 break;
 
             case 2:
                 if (!EmptyStack(S)) {
-                    Pop(&S, &X);
-                    printf("Elemen yang dihapus adalah %d\n", X);
+                    Pop(&S, &X); // Menghapus elemen teratas dari Stack
+                    printf("Elemen yang dihapus adalah %d\n", X); // Menampilkan nilai elemen yang dihapus
                 } else {
-                    printf("Stack kosong\n");
+                    printf("Stack kosong\n"); // Menampilkan pesan jika Stack kosong
                 }
                 break;
 
             case 3:
-                Print(S);
+                Print(S); // Mencetak elemen-elemen dalam Stack
                 break;
 
             case 4:
-                break;
+                break; // Keluar dari loop
 
             default:
-                printf("Pilihan tidak valid\n");
+                printf("Pilihan tidak valid\n"); // Menampilkan pesan jika pilihan tidak valid
                 break;
         }
-    } while (!Finished(Choice));
+    } while (!Finished(Choice)); // Melakukan perulangan selama program belum selesai
 
     return 0;
 }
-
