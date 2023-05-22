47fabd6e41d9c8246fa31ec598771ebc674ce413
darkdanu91 <117131935+darkdanu91@users.noreply.github.com>
2023-05-22 16:39:18 +0700

    Update Finished.cpp

--- Bahasa Pemrograman C/Semester 2/Program 10/Finished.cpp
+++ Bahasa Pemrograman C/Semester 2/Program 10/Finished.cpp
@@ -1,5 +1,7 @@
 #include "function.h"
 
+// Fungsi untuk memeriksa apakah program selesai
 bool Finished(int Choice) {
-    return (Choice == 4);
+    return (Choice == 4);  // Mengembalikan nilai true jika pilihan adalah 4, menunjukkan program selesai, false jika tidak
 }
+
