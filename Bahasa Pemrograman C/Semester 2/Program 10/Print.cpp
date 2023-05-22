bed36ce2685e8ece173cfd662822be4157c315de
darkdanu91 <117131935+darkdanu91@users.noreply.github.com>
2023-05-22 16:42:39 +0700

    Update Print.cpp

--- Bahasa Pemrograman C/Semester 2/Program 10/Print.cpp
+++ Bahasa Pemrograman C/Semester 2/Program 10/Print.cpp
@@ -1,15 +1,15 @@
 #include "function.h"
 
+// Fungsi untuk mencetak elemen-elemen dalam Stack
 void Print(Stack S) {
-    TInfo X; //-declare
-
+    TInfo X; // Deklarasi variabel X untuk menyimpan nilai elemen yang dihapus
     if (EmptyStack(S)) {
-        printf("Stack kosong\n");
+        printf("Stack kosong\n"); // Menampilkan pesan jika Stack kosong
     } else {
-        while (!EmptyStack(S)) {
-            Pop(&S, &X);
-            printf("Elemen yang dihapus = %d\n", X);
+        while (!EmptyStack(S)) { // Melakukan perulangan selama Stack tidak kosong
+            Pop(&S, &X); // Menghapus elemen teratas dari Stack dan menyimpan nilainya dalam variabel X
+            printf("Elemen yang dihapus = %d\n", X); // Menampilkan nilai elemen yang dihapus
         }
-
     }
 }
+
