b0e60ebee593c4ae069d70eea4bc1c9610efc832
darkdanu91 <117131935+darkdanu91@users.noreply.github.com>
2023-05-22 16:36:05 +0700

    Update CreateStack.cpp

--- Bahasa Pemrograman C/Semester 2/Program 10/CreateStack.cpp
+++ Bahasa Pemrograman C/Semester 2/Program 10/CreateStack.cpp
@@ -1,6 +1,6 @@
 #include "function.h"
 
+// Fungsi untuk menginisialisasi Stack
 void CreateStack(Stack *S) {
-    (*S).Top = 0;
+    (*S).Top = 0;  // Mengatur nilai Top menjadi 0, menandakan Stack kosong
 }
-
