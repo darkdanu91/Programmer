cd27530dcefaf8e80f70cf29d2881f8ab3a9861a
darkdanu91 <117131935+darkdanu91@users.noreply.github.com>
2023-05-22 16:40:25 +0700

    Update Pop.cpp

--- Bahasa Pemrograman C/Semester 2/Program 10/Pop.cpp
+++ Bahasa Pemrograman C/Semester 2/Program 10/Pop.cpp
@@ -1,6 +1,8 @@
 #include "function.h"
 
+// Fungsi untuk mengambil elemen teratas dari Stack (Pop)
 void Pop(Stack *S, TInfo *X) {
-    *X = (*S).ArrStack[(*S).Top];
-    (*S).Top -= 1; // (*S).Top = (*S).Top - 1
+    *X = (*S).ArrStack[(*S).Top]; // Menyimpan nilai elemen teratas dalam variabel X
+    (*S).Top -= 1; // Mengurangi nilai Top sebesar 1 untuk menggeser ke posisi elemen berikutnya
 }
+
